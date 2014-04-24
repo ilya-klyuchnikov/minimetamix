a0fbc198c66b6bf85ea663b0c8a3da19f31ededa
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2014-04-24 19:21:17 +0400

    Scala 2.11

--- build.sbt
+++ build.sbt
@@ -1,7 +1,7 @@
-scalaVersion := "2.10.3"
+scalaVersion := "2.11.0"
 
 name := "foetus"
 
 libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _ % "provided")
 
-libraryDependencies += "org.scalatest" %% "scalatest" % "2.0" % "test"
+libraryDependencies += "org.scalatest" %% "scalatest" % "2.1.3" % "test"
--- src/main/scala/parser.scala
+++ src/main/scala/parser.scala
@@ -9,16 +9,12 @@ import ast._
 object parser {
   def parseDefs(expr: Any): List[Def] = macro parseDefsImpl
 
-  def parseDefsImpl(c: Context)(expr: c.Expr[Any]): c.Expr[List[Def]] = {
+  def parseDefsImpl(c: blackbox.Context)(expr: c.Expr[Any]): c.Expr[List[Def]] = {
     import c.universe._
 
     object Name {
-      def unapply(name: Name): Option[String] = Some(name.decoded)
-    }
-
-    def trim(s: String) = s.split("\n").toList match {
-      case s1 :: Nil => s1
-      case s1 :: ss => s"$s1..."
+      def unapply(name: Name): Option[String] =
+        Some(name.decodedName.toString)
     }
 
     def parseTree(tree: Tree): Term = tree match {
@@ -35,55 +31,49 @@ object parser {
       case Apply(TypeApply(Ident(Name(ctrName)), _), args) =>
         App(ctrName, args.map(parseTree))
       case x =>
-        sys.error(s"UNKNOWN: ${showRaw(tree)}")
+        c.abort(tree.pos, "FOETUS. Unsupported syntax")
     }
 
     def parseCaseDef(caze: CaseDef): Branch = caze.pat match {
-      case Apply(ctr: TypeTree, bs) =>
-        val Ident(Name(name)) = ctr.original
-        Branch(Pat(name, bs.map {case Bind(Name(n), _) => n}), parseTree(caze.body))
+      case UnApply(Apply(Select(Ident(Name(name)), _), _), bs) =>
+        Branch(Pat(name, bs.map { case Bind(Name(n), _) => n }), parseTree(caze.body))
+      case UnApply(Apply(TypeApply(Select(Ident(Name(name)), _), _), _), bs) =>
+        Branch(Pat(name, bs.map { case Bind(Name(n), _) => n }), parseTree(caze.body))
       case x =>
-        sys.error(s"UNKNOWN: ${showRaw(caze.pat)}")
+        c.abort(caze.pat.pos, "FOETUS. Unsupported syntax")
     }
 
     def parseDef(name: String, vparamss: List[List[ValDef]], body: Tree): List[Def] = vparamss match {
       case params :: Nil =>
-        List(Def(name, params.map{_.name.decoded}, parseTree(body)))
-      case x =>
-        sys.error("curried functions are not supported in SLL")
+        List(Def(name, params.map(_.name.decodedName.toString), parseTree(body)))
+      case _ =>
+        c.abort(wrappingPos(vparamss.head), "FOETUS. Unsupported syntax")
     }
 
     def parseStmt(tree: Tree): List[Def] = tree match {
       case DefDef(modifiers, name, typeParams, params, returnType, body) =>
-        parseDef(name.decoded, params, body)
+        parseDef(name.decodedName.toString, params, body)
       case x =>
-        //println(s"WARNING ignoring unsupported definition: `${trim(tree.toString())}`")
+        //c.warning(tree.pos, "FOETUS. Ignored definition")
         Nil
     }
 
     def parseStmts(stmts: List[Tree]): List[Def] =
       stmts.flatMap(parseStmt)
 
-    def parseBlock(tree: Tree): c.Expr[List[Def]] =  tree match {
+    def parseBlock(tree: Tree): c.Expr[List[Def]] = tree match {
       case Block(stmts, _) =>
         val defs = parseStmts(stmts)
-        reify(qListDef(defs.map(d => qDef(d))).splice)
+        reify(qList(defs.map(qDef(_))).splice)
     }
 
-    def qs(s: String): Expr[String] =
+    def qs(s: String) =
       c.Expr[String](Literal(Constant(s)))
 
-    def qListString(xs: List[Expr[String]]): Expr[List[String]] =
-      xs.foldRight(reify{Nil: List[String]}){ (x, y) => reify{x.splice :: y.splice}}
-
-    def qListDef(xs: List[Expr[Def]]): Expr[List[Def]] =
-      xs.foldRight(reify{Nil: List[Def]}){ (x, y) => reify{x.splice :: y.splice}}
-
-    def qListTerm(xs: List[Expr[Term]]): Expr[List[Term]] =
-      xs.foldRight(reify{Nil: List[Term]}){ (x, y) => reify{x.splice :: y.splice}}
-
-    def qListBranch(xs: List[Expr[Branch]]): Expr[List[Branch]] =
-      xs.foldRight(reify{Nil: List[Branch]}){ (x, y) => reify{x.splice :: y.splice}}
+    def qList[T: WeakTypeTag](xs: List[Expr[T]]): Expr[List[T]] =
+      xs.foldRight(reify{ Nil: List[T] }) {
+        (x, y) => reify { x.splice :: y.splice }
+      }
 
     def qTerm(t: Term): Expr[Term] = t match {
       case Var(n) =>
@@ -92,29 +82,29 @@ object parser {
         }
       case Ctr(n, args) =>
         reify {
-          Ctr(qs(n).splice, qListTerm(args.map(t => qTerm(t))).splice )
+          Ctr(qs(n).splice, qList(args.map(qTerm(_))).splice )
         }
       case App(n, args) =>
         reify {
-          App(qs(n).splice, qListTerm(args.map(t => qTerm(t))).splice )
+          App(qs(n).splice, qList(args.map(qTerm(_))).splice )
         }
       case Case(sel, branches) =>
         reify {
-          Case(qTerm(sel).splice, qListBranch(branches.map(b => qBranch(b))).splice)
+          Case(qTerm(sel).splice, qList(branches.map(qBranch(_))).splice)
         }
     }
 
     def qBranch(b: Branch): Expr[Branch] = b match {
       case Branch(Pat(n, ps), body) =>
         reify {
-          Branch(Pat(qs(n).splice, qListString(ps.map({ s => qs(s) })).splice), qTerm(body).splice)
+          Branch(Pat(qs(n).splice, qList(ps.map(qs(_))).splice), qTerm(body).splice)
         }
     }
 
     def qDef(d: Def): Expr[Def] = d match {
       case Def(n, ps, body) =>
         reify {
-          Def(qs(n).splice, qListString(ps.map({ s => qs(s) })).splice , qTerm(body).splice)
+          Def(qs(n).splice, qList(ps.map(qs(_))).splice , qTerm(body).splice)
         }
     }
 
